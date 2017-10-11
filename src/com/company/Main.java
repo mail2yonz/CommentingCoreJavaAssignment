cc63ab6ae25728049b03cf134d44757219cc9fb9
Yonatan Mengesha <mail2yonz@gmail.com>
2017-10-11 09:22:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+/*
+ * Yonatan Mengesha
+ * October 10,2017
+ * Java BootCamp
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan= new Scanner (System.in);
+        //The below code creates a random object
+        Random random= new Random ();
+        long from=1;
+        long to =100;
+        int randomNumber= random.nextInt (to- from+1)+from;
+        //The below code initializes the guessedNumber
+        int guessedNumber=0;
+
+        //This code prints out from and to
+        System.out.printf("The number is between %d and %d.\n",from,to );
+
+        //The below does  the do while
+
+        do{
+            System.out.println("Guess what the number is:");
+            guessedNumber=scan.nextInt ();
+            if(guessedNumber>randomNumber)
+                System.out.println ("Your guess is too high!" );
+            else if(guessedNumber<randomNumber)
+                System.out.println ("Your guess is too low!" );
+            else
+                System.out.println ("You got it!" );
+
+
+        }while(guessedNumber!=randomNumber);
+
+    }
+}
